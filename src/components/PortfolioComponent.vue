b0320de522c369c48450be76ae42da731aad01c7
Vidya Rupak <vrupak@asu.edu>
2025-05-14 13:24:59 -0700

    added apple website homepage prototype

--- src/components/PortfolioComponent.vue
+++ src/components/PortfolioComponent.vue
@@ -143,6 +143,14 @@ export default {
           link: 'https://d2ckn2.axshare.com/?g=4&id=04c1i2&p=homepage',
           image: 'Camera-Store.png',
           description: 'Developed a high-fidelity e-commerce prototype for a camera store using Axure RP, featuring an intuitive UI with responsive hover effects and structured information architecture. Implemented comprehensive shopping features including mega menu navigation, featured product displays, and category organization. Created interactive elements like promotional banners and newsletter signup to enhance user engagement. Design prioritizes conversion optimization while maintaining strong SEO foundations through strategic content placement and accessibility considerations.'
+        },
+        {
+          id: 9,
+          title: 'Apple Website Prototype',
+          category: 'Web Design',
+          link: 'https://er3wb8.axshare.com/?id=2im3yc&p=home&sc=3',
+          image: 'Apple-Website.png',
+          description: 'Recreated the main page of apple.com using Axure RP, replicating its layout, fonts, icons, colors, menus, and core interactions. Published the interactive prototype on Axshare to match the original website’s look and feel.'
         }
       ]
     }
