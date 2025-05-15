808228ee11e5f32f5684270201f1a52a693b60d0
Vidya Rupak <vrupak@asu.edu>
2025-05-14 19:32:29 -0700

    updated query as viewer

--- api/github-contributions.js
+++ api/github-contributions.js
@@ -6,27 +6,27 @@ module.exports = async function (req, res) {
   if (!username) return res.status(400).json({ error: "username param missing." });
 
   try {
-    // ✅ 1. Profile info with octokit
     const { Octokit } = await import("@octokit/rest");
     const octokit = new Octokit({ auth: token });
 
     const user = (await octokit.rest.users.getByUsername({ username })).data;
 
-    // ✅ 2. Full contribution heatmap with graphql-request
     const { GraphQLClient, gql } = await import("graphql-request");
     const graphQLClient = new GraphQLClient("https://api.github.com/graphql", {
       headers: { Authorization: `Bearer ${token}` },
     });
 
     const query = gql`
-      query($login: String!) {
-        user(login: $login) {
-          contributionCalendar {
-            totalContributions
-            weeks {
-              contributionDays {
-                date
-                contributionCount
+      query {
+        viewer {
+          contributionsCollection {
+            contributionCalendar {
+              totalContributions
+              weeks {
+                contributionDays {
+                  date
+                  contributionCount
+                }
               }
             }
           }
@@ -34,7 +34,8 @@ module.exports = async function (req, res) {
       }
     `;
 
-    const data = await graphQLClient.request(query, { login: username });
+    const data = await graphQLClient.request(query);
+    const calendar = data.viewer.contributionsCollection.contributionCalendar;
 
     res.status(200).json({
       username: user.login,
@@ -43,8 +44,8 @@ module.exports = async function (req, res) {
       public_repos: user.public_repos,
       followers: user.followers,
       following: user.following,
-      totalContributions: data.user.contributionCalendar.totalContributions,
-      heatmapData: data.user.contributionCalendar.weeks.flatMap(week => week.contributionDays),
+      totalContributions: calendar.totalContributions,
+      heatmapData: calendar.weeks.flatMap(week => week.contributionDays),
     });
   } catch (error) {
     console.error(error);
